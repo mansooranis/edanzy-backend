9f77c93314aee21e631e7bfd8f42c91d667629d2
ArmaanKatyal <armaankatyal3@gmail.com>
2021-08-14 18:50:52 +0530

    updated the redis, not ready for use

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const redis = require('redis');
+const cookieParser = require('cookie-parser');
 require('dotenv/config');
 
 // Import Routes
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
 
-// Connect to DB
-mongoose.connect(process.env.DB_SRV, {useNewUrlParser : true},()=>console.log('connected to db!'));
-
 // MiddleWare
 app.use(express.json());
+app.use(bodyParser.urlencoded())
+app.use(cookieParser());
+
+// Connect to DB
+mongoose.connect(process.env.DB_SRV, {useNewUrlParser : true, useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true});
 
 // Route Middleware
-app.use('/api/user', authRoute);
-app.use('/api/posts', postRoute);
+app.use('/api/v1/user', authRoute);
+app.use('/api/v1/posts', postRoute);
 
+app.use((req, res)=>{
+    res.sendStatus(404);
+});
 
-app.listen(3000);
+app.listen(5000);
